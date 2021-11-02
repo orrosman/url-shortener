94210845fdd5c2c5973410a7b0bf12b19d1a4da8
Or Rosman <42673555+orrosman@users.noreply.github.com>
2021-11-02 15:12:34 +0200

    Get data from remote JSON BIN

--- api/routers/urlRouter.js
+++ api/routers/urlRouter.js
@@ -1,16 +1,18 @@
 const express = require('express');
 const router = express.Router();
+const axios = require('axios');
 const { v4: uuidv4 } = require('uuid');
 const fs = require('fs');
 const path = require('path');
 module.exports = router;
 
 const BASE_URL = 'localhost:8080';
+const DATABASE_URL = 'https://api.jsonbin.it/bins/hvRVYJVX';
 
 router.put('/', (req, res) => {
 	const longUrl = req.body.url;
 	const urlObj = buildUrlObject(longUrl);
-
+	getDataFromDatabase();
 	res.send(urlObj.shortUrl);
 });
 
@@ -24,4 +26,9 @@ function buildUrlObject(longUrl) {
 	};
 
 	return urlObject;
+}
+
+async function getDataFromDatabase() {
+	const response = await axios.get(DATABASE_URL);
+	return response.data;
 }
