40c78c92cece92ffeca7a53aae31db37bc0c65eb
Or Rosman <42673555+orrosman@users.noreply.github.com>
2021-11-03 10:25:04 +0200

    validate url from user

--- api/routers/urlRouter.js
+++ api/routers/urlRouter.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const axios = require('axios');
 const { v4: uuidv4 } = require('uuid');
+const validUrl = require('valid-url');
 const fs = require('fs');
 const path = require('path');
 module.exports = router;
@@ -9,17 +10,25 @@ module.exports = router;
 const BASE_URL = 'localhost:8080';
 
 router.put('/', (req, res) => {
-	const longUrl = req.body.url;
-	const urlObj = buildUrlObject(longUrl);
-	addToDataBase(urlObj);
-	res.send(urlObj.shortUrl);
+	try {
+		const longUrl = req.body.url;
+		if (!longUrl) {
+			throw { code: 400, msg: 'No URL was provided' };
+		}
+		if (!validUrl.isUri(longUrl)) {
+			throw { code: 400, msg: 'The URL that was provided is not valid' };
+		}
+		const urlObj = buildUrlObject(longUrl);
+		addToDataBase(urlObj);
+		res.send(urlObj.shortUrl);
+	} catch {
+		next(error);
+	}
 });
 
 router.get('/:id', async (req, res) => {
 	const id = req.params.id;
 	const longUrl = await getLongUrl(id);
-	// console.log(longUrl);
-	// return res.send('apple.com');
 	return res.redirect(longUrl);
 });
 
